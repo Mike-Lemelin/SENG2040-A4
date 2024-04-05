eb8f962ffd255c1730386ff20bad79e13b0d6875
michael-lemelin <mikelemelin1515@gmail.com>
2024-04-05 17:13:41 -0400

    Completed first Ajax call

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -3,3 +3,15 @@ console.log('hello world')
 const hellowWorldBox = document.getElementById('hello-world')
 
 hellowWorldBox.innerHTML = 'hello <b>world'
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('success', response)
+        hellowWorldBox.textContent = response.text
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
