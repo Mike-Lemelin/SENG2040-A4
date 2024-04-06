5a5ebefa39e25628d5af9f31858b1db5f5096bc0
michael-lemelin <mikelemelin1515@gmail.com>
2024-04-06 03:02:18 -0400

    Added code to create view for image uploading

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -6,7 +6,8 @@ from .views import (
     like_unlike_post,
     post_detail_data_view,
     delete_post,
-    update_post
+    update_post,
+    image_upload_view
 )
 
 app_name = 'posts'
@@ -15,8 +16,9 @@ urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
+    path('upload/', image_upload_view, name='image-upload'),
     path('<pk>/', post_detail, name='post-detail'),
     path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
     path('<pk>/update/', update_post, name='post-update'),
-    path('<pk>/delete/', delete_post, name='post-delete')
+    path('<pk>/delete/', delete_post, name='post-delete'),
 ]
