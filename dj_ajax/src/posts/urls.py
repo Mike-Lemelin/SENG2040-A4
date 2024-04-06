86b9afd9f6824e2336da8e79480958e391bedf58
michael-lemelin <mikelemelin1515@gmail.com>
2024-04-06 01:36:07 -0400

    Added final functionality to update button for posts

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -4,7 +4,9 @@ from .views import (
     load_post_data_view,
     post_detail,
     like_unlike_post,
-    post_detail_data_view
+    post_detail_data_view,
+    delete_post,
+    update_post
 )
 
 app_name = 'posts'
@@ -14,5 +16,7 @@ urlpatterns = [
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
     path('<pk>/', post_detail, name='post-detail'),
-    path('<pk>/data/', post_detail_data_view, name='post-detail-data')
+    path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
+    path('<pk>/update/', update_post, name='post-update'),
+    path('<pk>/delete/', delete_post, name='post-delete')
 ]
